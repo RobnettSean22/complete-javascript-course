de7347733b8a6572a64e3400f7968f5c1369080f
RobnettSean22 <robnettsean22@gmail.com>
2019-09-22 10:37:42 -0700

    first commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,61 @@
+// varibles and data types 
+// varible naming and rules
+
+// var firstName = 'John';
+// console.log(firstName)
+
+// var lastName = 'Smith';
+// var age = 24;
+
+// var fullAge = true;
+// console.log(fullAge)
+
+// var job = 'Teacher';
+// console.log(job)
+
+// var year = 3;
+
+// var johnMark = 'John and Mark';
+
+/*********************************************
+ * varible mutation and type coercion
+ */
+
+
+//  var firstName = 'John';
+//  var age = 28;
+// // type coercion
+//     console.log(firstName + ' ' + age) 
+
+//  //type coercion
+//     var job , isMariied;
+//     job = 'teacher';
+//     isMariied = false;
+
+//     console.log(firstName + 'is a ' + age + ' year old ' + job + '. Is he married?' + ' ' + isMariied )
+
+//  //Varible mutation
+
+//     age = 'twenty eight';
+//     job = 'driver';
+
+// // alert() alternative to console.log()
+
+//     alert(firstName + 'is a ' + age + ' year old ' + job + '. Is he married?' + ' ' + isMariied )
+// // prompt() makes a pop in the window allowing one to enter information 
+ 
+//     var lastName = prompt('What is his last name?')
+//     console.log(firstName + ' ' + lastName)
+
+ /****************************************************************
+  * Basci Operators
+  *  */   
+    
+    var year, yearJohn, yearMark;
+  
+    now = 2019
+    yearJohn = now - 28;
+    yearMark = now - 33;
+
+    console.log(now + 2)
+    
